aa04f2e3283c504b814c8ee7be4a200c17d8ea05
Michael B <michaelbor2015@gmail.com>
2017-01-29 00:24:37 -0800

    activities

---
+++ app/src/main/java/michael/com/firebaseapp/activity/LogInActivity.java
@@ -0,0 +1,19 @@
+package michael.com.firebaseapp.activity;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
+import michael.com.firebaseapp.R;
+
+/**
+ * Created by Mikhail on 1/28/17.
+ */
+
+public class LogInActivity extends AppCompatActivity{
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_log_in);    }
+}
