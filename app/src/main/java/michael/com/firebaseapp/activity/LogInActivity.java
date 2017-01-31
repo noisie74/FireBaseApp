670cc5287282ebdf315b302d99eca1b27950660a
Michael B <michaelbor2015@gmail.com>
2017-01-30 22:19:44 -0800

    complete activities

--- app/src/main/java/michael/com/firebaseapp/activity/LogInActivity.java
+++ app/src/main/java/michael/com/firebaseapp/activity/LogInActivity.java
@@ -1,19 +1,90 @@
 package michael.com.firebaseapp.activity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
 import michael.com.firebaseapp.R;
 
 /**
  * Created by Mikhail on 1/28/17.
  */
 
-public class LogInActivity extends AppCompatActivity{
+public class LogInActivity extends AppCompatActivity {
+
+    @BindView(R.id.emailField)
+    public EditText emailEditText;
+    @BindView(R.id.passwordField)
+    public EditText passwordEditText;
+    @BindView(R.id.loginButton)
+    public Button logInButton;
+    @BindView(R.id.signUpText)
+    protected TextView signUpTextView;
+    private FirebaseAuth mFirebaseAuth;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_log_in);    }
+        setContentView(R.layout.activity_log_in);
+        mFirebaseAuth = FirebaseAuth.getInstance();
+        ButterKnife.bind(this);
+        setButtonClickListners();
+    }
+
+    private void setButtonClickListners() {
+        signUpTextView.setOnClickListener(v -> {
+            Intent intent = new Intent(LogInActivity.this, SignUpActivity.class);
+            startActivity(intent);
+        });
+
+        logInButton.setOnClickListener(v -> {
+            String email = emailEditText.getText().toString();
+            String password = passwordEditText.getText().toString();
+
+            email = email.trim();
+            password = password.trim();
+
+            if (email.isEmpty() || password.isEmpty()) {
+                AlertDialog.Builder builder = new AlertDialog.Builder(LogInActivity.this);
+                builder.setMessage(R.string.login_error_message)
+                        .setTitle(R.string.login_error_title)
+                        .setPositiveButton(android.R.string.ok, null);
+                AlertDialog dialog = builder.create();
+                dialog.show();
+
+            } else {
+                mFirebaseAuth.signInWithEmailAndPassword(email, password)
+                        .addOnCompleteListener(LogInActivity.this, task -> {
+                            if (task.isSuccessful()) {
+                                Intent intent = new Intent(LogInActivity.this, MainActivity.class);
+                                intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+                                intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK);
+                                startActivity(intent);
+                            } else {
+                                AlertDialog.Builder builder = new AlertDialog.Builder(LogInActivity.this);
+                                builder.setMessage(task.getException().getMessage())
+                                        .setTitle(R.string.login_error_title)
+                                        .setPositiveButton(android.R.string.ok, null);
+                                AlertDialog dialog = builder.create();
+                                dialog.show();
+                            }
+                        });
+            }
+        });
+    }
+
 }
--- app/src/main/java/michael/com/firebaseapp/activity/SignUpActivity.java
+++ app/src/main/java/michael/com/firebaseapp/activity/SignUpActivity.java
@@ -1,9 +1,22 @@
 package michael.com.firebaseapp.activity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
 import michael.com.firebaseapp.R;
 
 /**
@@ -12,8 +25,57 @@ import michael.com.firebaseapp.R;
 
 public class SignUpActivity extends AppCompatActivity {
 
+    private FirebaseAuth mFirebaseAuth;
+    @BindView(R.id.passwordField)
+    public EditText passwordEditText;
+    @BindView(R.id.emailField)
+    public EditText emailEditText;
+    @BindView(R.id.signupButton)
+    public Button signUpButton;
+
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_sign_up);    }
+        setContentView(R.layout.activity_sign_up);
+        ButterKnife.bind(this);
+        setButtonClickListners();
+    }
+
+    private void setButtonClickListners() {
+
+        signUpButton.setOnClickListener(v -> {
+            String password = passwordEditText.getText().toString();
+            String email = emailEditText.getText().toString();
+
+            password = password.trim();
+            email = email.trim();
+
+            if (password.isEmpty() || email.isEmpty()) {
+                AlertDialog.Builder builder = new AlertDialog.Builder(SignUpActivity.this);
+                builder.setMessage(R.string.signup_error_message)
+                        .setTitle(R.string.signup_error_title)
+                        .setPositiveButton(android.R.string.ok, null);
+                AlertDialog dialog = builder.create();
+                dialog.show();
+            } else {
+                mFirebaseAuth.createUserWithEmailAndPassword(email, password)
+                        .addOnCompleteListener(SignUpActivity.this, task -> {
+                            if (task.isSuccessful()) {
+                                Intent intent = new Intent(SignUpActivity.this, MainActivity.class);
+                                intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+                                intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK);
+                                startActivity(intent);
+                            } else {
+                                AlertDialog.Builder builder = new AlertDialog.Builder(SignUpActivity.this);
+                                builder.setMessage(task.getException().getMessage())
+                                        .setTitle(R.string.login_error_title)
+                                        .setPositiveButton(android.R.string.ok, null);
+                                AlertDialog dialog = builder.create();
+                                dialog.show();
+                            }
+                        });
+            }
+
+        });
+    }
 }
